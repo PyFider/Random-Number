d1e1850b621a775da98f39ef3667b0bcfc95ea75
Eurry <92621710+PyFider@users.noreply.github.com>
2022-05-22 11:21:08 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib> 
+#include <iostream>
+# include <Windows.h>
+
+using namespace std;
+
+int main() 
+{ 
+	while (true) {
+		
+	    int i;
+	    i = rand()/27; 
+	    cout << i << endl;
+	    Sleep(292);
+	    system("cls");
+	}
+}
